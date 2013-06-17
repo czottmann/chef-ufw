f3fbbd32c84fea57f53c28aaed3cfaf4f2f03078
Matteo Alessani <alessani@gmail.com>
2013-06-17 17:11:09 +0200

    added postgres!

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,3 +8,10 @@ execute "enable UFW" do
   user "root"
   command "ufw allow ssh; echo y | ufw enable" # "Command may disrupt existing ssh connections. Proceed with operation (y|n)?"
 end
+
+template "/etc/ufw/applications.d/postgresql" do
+  source "postgresql"
+  owner "root"
+  group "root"
+  mode 00644
+end
