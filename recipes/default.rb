343f1b0d85eefce3dc54c16e03cf5b3bed78364b
Matteo Alessani <alessani@gmail.com>
2013-06-18 14:43:58 +0200

    removed ufw.conf for upstart

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,13 +9,6 @@ execute "enable UFW" do
   command "ufw allow ssh; echo y | ufw enable" # "Command may disrupt existing ssh connections. Proceed with operation (y|n)?"
 end
 
-template "/etc/init/ufw.conf" do
-  source "ufw.conf"
-  owner "root"
-  group "root"
-  mode 00644
-end
-
 template "/etc/ufw/applications.d/postgresql" do
   source "postgresql"
   owner "root"
